749e4326986f976450ee0aa8d8c1ce7b000c482f
Ivan Bobrov <bobrov.i.i@yandex.ru>
2024-07-02 23:19:13 +0300

    Сделал Отзывы: модель, админку, сериализатор, вьюху, маршруты

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
 
-# Create your views here.
+from reviews.models import Title
+
+from .permissions import IsAuthorOrAdminOrModerator
+from .serializers import ReviewSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    pagination_class = LimitOffsetPagination
+    permission_classes = (IsAuthorOrAdminOrModerator,)
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user, title=title)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,55 @@
+"""Модели приложения reviews."""
+
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Title(models.Model):
+    """Модель описывающая публикации."""
+    pass
+
+
+class Review(models.Model):
+    """Модель описывающая отзывы к публикациям."""
+
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Произведение'
+    )
+    text = models.TextField(
+        verbose_name='Текст отзыва'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор'
+    )
+    score = models.PositiveSmallIntegerField(
+        verbose_name='Оценка',
+        validators=(
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ),
+        error_messages={'validators': 'Оценка может быть от 1 до 10!'}
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата добавления отзыва'
+    )
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(fields=('author', 'title'),
+                                    name='unique_review'),)
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.text[:15]
