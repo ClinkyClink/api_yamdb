c84f701e9e0328d5d83dbfff921d893fcc1a43ae
Dmitrii Kuksin <snymlik@gmail.com>
2024-07-03 22:20:36 +0700

    вот так

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,7 @@
 """Маршруты приложения api."""
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
 from .views import ReviewViewSet
 from users.views import SignupView, TokenView, UserViewSet
 
