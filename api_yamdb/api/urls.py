1c988db6c4bd7f9cb3ec2a7e8cc52d9a21d73833
Евгений Аркатов <thejekaarkatov@yandex.ru>
2024-07-03 18:26:43 +0300

    Добавил ендпоинты cat, gen, tit

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,7 +2,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import ReviewViewSet
+from .views import ReviewViewSet, TitleViewSet, GenreViewSet, CategoryViewSet
 
 router_v1 = DefaultRouter()
 router_v1.register(
@@ -10,6 +10,9 @@ router_v1.register(
     ReviewViewSet,
     basename='review'
 )
+router_v1.register('categories', CategoryViewSet, basename='category')
+router_v1.register('genres', GenreViewSet, basename='genre')
+router_v1.register('titles', TitleViewSet, basename='title')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls))
