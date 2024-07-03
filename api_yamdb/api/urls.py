2d63b0723dd0a0255cd137597ce91ee3e3db5030
Dmitrii Kuksin <snymlik@gmail.com>
2024-07-03 20:40:21 +0700

    так второй конфликт решен

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,14 +1,21 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
+from .views import ReviewViewSet
 from users.views import SignupView, TokenView, UserViewSet
 
 app_name = 'api'
 
-router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='user')
+router_v1 = DefaultRouter()
+router_v1.register(r'users', UserViewSet, basename='user')
+router_v1.register(
+    r'^titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='review'
+)
 
 urlpatterns = [
     path('v1/auth/signup/', SignupView.as_view(), name='signup'),
     path('v1/auth/token/', TokenView.as_view(), name='token'),
-    path('v1/', include(router.urls)),
+    path('v1/', include(router_v1.urls)),
 ]
