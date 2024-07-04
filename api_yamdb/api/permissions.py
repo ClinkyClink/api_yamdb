48ea85b3b2ebf1fd529f8435cc6364ef13672b38
Евгений Аркатов <thejekaarkatov@yandex.ru>
2024-07-04 23:40:28 +0300

    вернул проверку на автора

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,13 +5,10 @@ class IsAuthorOrAdminOrModerator(permissions.BasePermission):
     """Проверка на автора, админа или модератора."""
 
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        if request.method in ['POST', 'PUT', 'DELETE', 'PATCH']:
-            return request.user.is_authenticated and request.user.is_admin
-
-        return request.user and request.user.is_authenticated
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user and request.user.is_authenticated
+        )
 
     def has_object_permission(self, request, view, obj):
         return (
